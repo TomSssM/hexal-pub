7fe657e9dbf47b86be12046940b40b3f7505b69f
TomSssM <ilyashome3@gmail.com>
2019-03-01 00:29:40 +0300

    fix: validate code

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const cont = document.querySelector('.js-container');
 
 document.addEventListener('click', function(e) {
   e.preventDefault();
+
   if(e.target.closest('.js-arrow-down')) cont.scrollIntoView({block:'start', behavior:'smooth'});
 });
-
